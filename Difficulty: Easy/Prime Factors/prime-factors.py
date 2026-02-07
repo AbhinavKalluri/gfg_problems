b1a09443886ca6d981cffb5ae536834259c55dfd
AbhinavKalluri <Kalluriabhinav972@gmail.com>
2026-02-07 17:43:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Factors/prime-factors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def primeFac(self, n):
+        # code here
+        res=[]
+        if n%2==0:
+            res.append(2)
+            while n%2==0:
+                n//=2
+        i=3
+        while i*i<=n:
+            if n%i==0:
+                res.append(i)
+                while n%i==0:
+                    n//=i
+            i+=2
+        if n>1:
+            res.append(n)
+        return res
+        
+        
